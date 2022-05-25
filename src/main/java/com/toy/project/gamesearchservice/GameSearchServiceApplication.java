11e9b7c3edfa1184526ba11b994c81c10290c24d
노태환 <thnoh93@naver.com>
2022-05-25 17:39:25 +0900

    first commit

---
+++ src/main/java/com/toy/project/gamesearchservice/GameSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toy.project.gamesearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/project/gamesearchservice/GameSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.project.gamesearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
